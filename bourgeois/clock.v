0f55c9f8b34559d541483ea28344446066767861
feipu123 <czpcxfzwq@126.com>
2013-06-06 19:03:46 +0800

    finish inst_mem and data_mem

--- bourgeois/clock.v
+++ bourgeois/clock.v
@@ -3,6 +3,11 @@
 module clock(out);
   output out;
   reg out;
+  
+  initial begin
+    out = 0;
+  end
+  
   always begin
     #1 out = ~out;
   end
--- bourgeois/datamem.v
+++ bourgeois/datamem.v
@@ -7,11 +7,15 @@ module datamem(clk, in, out);
   wire [`BYTE_SIZE-1:0]in;
   reg [`BLOCK_SIZE-1:0]out;
   reg [`BYTE_SIZE-1:0]data[0:`DATA_MEM_SIZE];
+  reg[`WORD_SIZE-1:0] i;
+    
   initial fork
   $readmemb("ram_data.txt", data);
   join
-  always @(posedge clk)
-    fork
-      
-    join
+  
+  always @(posedge clk) begin
+      out = 0;
+      for (i = 0; i < `BLOCK_SIZE / `BYTE_SIZE; i = i + 1) 
+        out = (out << `BYTE_SIZE) + data[in + i];
+  end
 endmodule
