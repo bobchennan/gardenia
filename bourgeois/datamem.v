391cb9c95e2aa77875fdec8ce1346eb099ff1d16
feipu123 <czpcxfzwq@126.com>
2013-06-08 01:03:51 +0800

    revise datamem

--- bourgeois/datamem.v
+++ bourgeois/datamem.v
@@ -1,9 +1,9 @@
 `include "define.v"
 
-module datamem(clk, in, writable, write, out1, out2);
+module datamem(clk, in, readable, writable, write, out1, out2);
   input clk; 
   input[`WORD_SIZE-1:0] in;
-  input writable;
+  input readable, writable;
   input[`BLOCK_SIZE-1:0] write;
   output out1, out2;
   reg[`BLOCK_SIZE-1:0] out1, out2;
@@ -12,21 +12,21 @@ module datamem(clk, in, writable, write, out1, out2);
   reg[`WORD_SIZE-1:0] i;
     
   initial fork
-  $readmemb("ram_data.txt", data);
+    $readmemb("ram_data.txt", data);
   join
   
   always @(posedge clk) begin
-      if (writable == 0) begin
-      out1 = 0;
-      for (i = 0; i < `BLOCK_SIZE / `BYTE_SIZE; i = i + 1) 
-        out1 = (out1 << `BYTE_SIZE) + data[in + i];
-      out2 = 0;
-      for (i = 0; i < `BLOCK_SIZE / `BYTE_SIZE; i = i + 1)
-        out2 = (out2 << `BYTE_SIZE) + data[in + `BLOCK_SIZE / `BYTE_SIZE + i];
-      end
-      else begin
+      if (writable == 1) begin
+	    for (i = 0; i < `BLOCK_SIZE / `BYTE_SIZE; i = i + 1) 
+          data[(in >> 7 << 7) + i] = (write >> (`BLOCK_SIZE - `BYTE_SIZE * (i+1))) & 8'b11111111;
+	  end
+      if (readable == 1) begin
+        out1 = 0;
         for (i = 0; i < `BLOCK_SIZE / `BYTE_SIZE; i = i + 1) 
-          data[in + i] = (write >> (`BLOCK_SIZE - `BYTE_SIZE * (i+1))) & 8'b11111111;
+          out1 = (out1 << `BYTE_SIZE) + data[(in >> 7 << 7) + i];
+        out2 = 0;
+        for (i = 0; i < `BLOCK_SIZE / `BYTE_SIZE; i = i + 1)
+          out2 = (out2 << `BYTE_SIZE) + data[(in >> 7 << 7) + `BLOCK_SIZE / `BYTE_SIZE + i];
       end
   end
 endmodule
--- bourgeois/instmem.v
+++ bourgeois/instmem.v
@@ -3,8 +3,7 @@
 module instmem(clk, in, readable, writable, write, out1, out2);
   input clk;
   input[`WORD_SIZE-1:0] in; // address
-  input readable;
-  input writable;
+  input readable, writable;
   input[`BLOCK_SIZE-1:0] write;
   output out1, out2;
   reg[`BLOCK_SIZE-1:0] out1, out2;
@@ -17,18 +16,17 @@ module instmem(clk, in, readable, writable, write, out1, out2);
   end
   
   always @(posedge clk) begin
-    if(writable==1)begin
+    if (writable == 1) begin
         for (i = 0; i < `BLOCK_SIZE / `BYTE_SIZE; i = i + 1) 
           inst[(in >> 7 << 7) + i] = (write >> (`BLOCK_SIZE - `BYTE_SIZE * (i+1))) & 8'b11111111;
     end
     if (readable == 1) begin
       out1 = 0;
-      for (i = 0; i < `BLOCK_SIZE / `BYTE_SIZE; i = i + 1) begin
+      for (i = 0; i < `BLOCK_SIZE / `BYTE_SIZE; i = i + 1) 
         out1 = (out1 << `BYTE_SIZE) + inst[(in >> 7 << 7) + i];
-      end
       out2 = 0;
       for (i = 0; i < `BLOCK_SIZE / `BYTE_SIZE; i = i + 1)
         out2 = (out2 << `BYTE_SIZE) + inst[(in >> 7 << 7) + `BLOCK_SIZE / `BYTE_SIZE + i];
-      end
+    end
   end
 endmodule
