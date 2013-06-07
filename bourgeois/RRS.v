d793413ac0d3d997cbf13e151162229da24ef250
feipu123 <czpcxfzwq@126.com>
2013-06-07 23:12:37 +0800

    remove RS, complete RRS

--- bourgeois/RRS.v
+++ bourgeois/RRS.v
@@ -1,4 +1,17 @@
 `include "define.v"
 
 module RRS();
+  input[5:0] r; // 64 registers
+  input writable; // write(1) or read(0)
+  input[`UNIT_SIZE-1:0] write;
+  output[`UNIT_SIZE-1:0] out; // which unit is using this register
+  
+  reg[`UNIT_SIZE-1:0] rrs[63:0];
+  
+  always begin
+    if (writable == 1) 
+	  rrs[r] = write;
+	else
+	  out = rrs[r];
+  end
 endmodule
