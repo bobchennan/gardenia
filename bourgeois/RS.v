279e4acc554e24e0a18bafc217a08b6f4a617729
feipu123 <czpcxfzwq@126.com>
2013-06-21 02:52:22 +0800

    rewrite RS and delete cdb no "bgt" problem but "x"'s appear if "run 400000"

--- bourgeois/RS.v
+++ bourgeois/RS.v
@@ -25,10 +25,7 @@ module RS(clk, unit, reg1, reg2, reg3, hasimm, imm, enable, out, regread, regin,
   //mv(register has its value) : 01111111
   reg[`GENERAL_RS_SIZE-1:0] add[0:32-1], mul[0:32-1], lw[0:96-1];
   reg[`SW_RS_SIZE-1:0] sw[0:32-1];
-  reg[`UNIT_SIZE + `WORD_SIZE:0 - 1] cdb;
-  reg cdbchange;
   initial begin
-    cdbchange = 0;
     for (k = 0; k < 32; k = k + 1)
       add[k] = 0;
     for (k = 0; k < 32; k = k + 1)
@@ -39,6 +36,18 @@ module RS(clk, unit, reg1, reg2, reg3, hasimm, imm, enable, out, regread, regin,
       sw[k] = 0;
   end
   
+  //Register Result Status
+  reg[`UNIT_SIZE-1:0] rrs[63:0];
+  reg[`WORD_SIZE-1:0] rf[63:0];
+  assign regout = rrs[regin];
+  assign regoutrf = rf[regin];
+  initial begin
+    for (k = 0; k < 64; k = k + 1) begin
+      rrs[k] = 8'b01111111;
+      rf[k] = 0;
+    end
+  end
+  
   genvar geni;
   //ALU for add
   generate for (geni = 0; geni < 32; geni = geni + 1) begin:czpadd
@@ -47,25 +56,63 @@ module RS(clk, unit, reg1, reg2, reg3, hasimm, imm, enable, out, regread, regin,
     reg[`GENERAL_RS_SIZE-1:0] tmp2;
     assign tmp = add[geni];
     ADD addd(addout, $signed(tmp[81:50]), $signed(tmp[49:18]));
+    integer l;
     always @(posedge clk) begin
       if (tmp[1:1] == 1 && tmp[0:0] == 1) begin
-        cdb = ((8'b00100000 + geni) << `WORD_SIZE) + $unsigned(addout);
+        for (l = 0; l < 32; l = l + 1) 
+          if (add[l] >> (`GENERAL_RS_SIZE - 1) == 1) begin
+            if (8'b00100000 + geni == (add[l] >> 10) & 8'b11111111 && (add[l] >> 1) & 1'b1 == 0) begin
+              tmp2 = add[l] & ((1 << 50) - 1);
+              add[l] = (((add[l] >> 82 << 32) + $unsigned(addout) << 50) + tmp2) | 2'b10;
+            end
+            if (8'b00100000 + geni == (add[l] >> 2) & 8'b11111111 && add[l] & 1'b1 == 0) begin
+              tmp2 = add[l] & ((1 << 18) - 1);
+              add[l] = (((add[l] >> 50 << 32) + $unsigned(addout) << 18) + tmp2) | 2'b01;
+            end
+          end
+        for (l = 0; l < 32; l = l + 1) 
+          if (mul[l] >> (`GENERAL_RS_SIZE - 1) == 1) begin
+            if (8'b00100000 + geni == (mul[l] >> 10) & 8'b11111111 && (mul[l] >> 1) & 1'b1 == 0) begin
+              tmp2 = add[l] & ((1 << 50) - 1);
+              mul[l] = (((mul[l] >> 82 << 32) + $unsigned(addout) << 50) + tmp2) | 2'b10;
+            end
+            if (8'b00100000 + geni == (mul[l] >> 2) & 8'b11111111 && mul[l] & 1'b1 == 0) begin
+              tmp2 = mul[l] & ((1 << 18) - 1);
+              mul[l] = (((mul[l] >> 50 << 32) + $unsigned(addout) << 18) + tmp2) | 2'b01;
+            end
+          end
+        for (l = 0; l < 96; l = l + 1) 
+          if (lw[l] >> (`GENERAL_RS_SIZE - 1) == 1) begin
+            if (8'b00100000 + geni == (lw[l] >> 10) & 8'b11111111 && (lw[l] >> 1) & 1'b1 == 0) begin
+              tmp2 = lw[l] & ((1 << 50) - 1);
+              lw[l] = (((lw[l] >> 82 << 32) + $unsigned(addout) << 50) + tmp2) | 2'b10;
+            end
+            if (8'b00100000 + geni == (lw[l] >> 2) & 8'b11111111 && lw[l] & 1'b1 == 0) begin
+              tmp2 = lw[l] & ((1 << 18) - 1);
+              lw[l] = (((lw[l] >> 50 << 32) + $unsigned(addout) << 18) + tmp2) | 2'b01;
+            end
+          end
+        for (l = 0; l < 32; l = l + 1) 
+          if (sw[l] >> (`SW_RS_SIZE - 1) == 1) begin
+            if (8'b00100000 + geni == (sw[l] >> 19) & 8'b11111111 && (sw[l] >> 2) & 1'b1 == 0) begin
+              tmp2 = sw[l] & ((1 << 91) - 1);
+              sw[l] = (((sw[l] >> 123 << 32) + $unsigned(addout) << 91) + tmp2) | 3'b100;
+            end
+            if (8'b00100000 + geni == (sw[l] >> 11) & 8'b11111111 && (tmp >> 1) & 1'b1 == 0) begin
+              tmp2 = sw[l] & ((1 << 59) - 1);
+              sw[l] = (((sw[l] >> 91 << 32) + $unsigned(addout) << 59) + tmp2) | 3'b010;
+            end
+            if (8'b00100000 + geni == (tmp >> 3) & 8'b11111111 && tmp & 1'b1 == 0) begin
+              tmp2 = sw[l] & ((1 << 27) - 1);
+              sw[l] = (((sw[l] >> 59 << 32) + $unsigned(addout) << 27) + tmp2) | 3'b001;
+            end
+          end
+        for (l = 0; l < 64; l = l + 1) 
+          if (rrs[l] == 8'b00100000 + geni) begin
+            rrs[l] = 8'b01111111;
+            rf[l] = addout;
+          end
         add[geni] = 0;
-        cdbchange = 1;
-        $display("cdb change");
-      end
-    end
-    always @(cdbchange) begin
-      if (cdbchange && tmp >> (`GENERAL_RS_SIZE - 1) == 1) begin
-        if (cdb >> `WORD_SIZE == (tmp >> 10) & 8'b11111111 && (tmp >> 1) & 1'b1 == 0) begin
-          tmp2 = add[geni] & ((1 << 50) - 1);
-          add[geni] = (((add[geni] >> 82 << 32) + (cdb & `MAX_UNSIGN_INT) << 50) + tmp2) | 2'b10;
-        end
-        if (cdb >> `WORD_SIZE == (tmp >> 2) & 8'b11111111 && tmp & 1'b1 == 0) begin
-          tmp2 = add[geni] & ((1 << 18) - 1);
-          add[geni] = (((add[geni] >> 50 << 32) + (cdb & `MAX_UNSIGN_INT) << 18) + tmp2) | 2'b01;
-        end
-        cdbchange = 0;
       end
     end
   end
@@ -77,25 +124,64 @@ module RS(clk, unit, reg1, reg2, reg3, hasimm, imm, enable, out, regread, regin,
     wire signed[`WORD_SIZE-1:0] mulout;
     reg[`GENERAL_RS_SIZE-1:0] tmp2;
     assign tmp = mul[geni];
+    integer l;
     MUL mull(mulout, $signed(tmp[81:50]), $signed(tmp[49:18]));
     always @(posedge clk) begin
       if (tmp[1:1] == 1 && tmp[0:0] == 1) begin
-        cdb = ((8'b01000000 + geni) << `WORD_SIZE) + $unsigned(mulout);
+        for (l = 0; l < 32; l = l + 1) 
+          if (add[l] >> (`GENERAL_RS_SIZE - 1) == 1) begin
+            if (8'b01000000 + geni == (add[l] >> 10) & 8'b11111111 && (add[l] >> 1) & 1'b1 == 0) begin
+              tmp2 = add[l] & ((1 << 50) - 1);
+              add[l] = (((add[l] >> 82 << 32) + $unsigned(mulout) << 50) + tmp2) | 2'b10;
+            end
+            if (8'b01000000 + geni == (add[l] >> 2) & 8'b11111111 && add[l] & 1'b1 == 0) begin
+              tmp2 = add[l] & ((1 << 18) - 1);
+              add[l] = (((add[l] >> 50 << 32) + $unsigned(mulout) << 18) + tmp2) | 2'b01;
+            end
+          end
+        for (l = 0; l < 32; l = l + 1) 
+          if (mul[l] >> (`GENERAL_RS_SIZE - 1) == 1) begin
+            if (8'b01000000 + geni == (mul[l] >> 10) & 8'b11111111 && (mul[l] >> 1) & 1'b1 == 0) begin
+              tmp2 = add[l] & ((1 << 50) - 1);
+              mul[l] = (((mul[l] >> 82 << 32) + $unsigned(mulout) << 50) + tmp2) | 2'b10;
+            end
+            if (8'b01000000 + geni == (mul[l] >> 2) & 8'b11111111 && mul[l] & 1'b1 == 0) begin
+              tmp2 = mul[l] & ((1 << 18) - 1);
+              mul[l] = (((mul[l] >> 50 << 32) + $unsigned(mulout) << 18) + tmp2) | 2'b01;
+            end
+          end
+        for (l = 0; l < 96; l = l + 1) 
+          if (lw[l] >> (`GENERAL_RS_SIZE - 1) == 1) begin
+            if (8'b01000000 + geni == (lw[l] >> 10) & 8'b11111111 && (lw[l] >> 1) & 1'b1 == 0) begin
+              tmp2 = lw[l] & ((1 << 50) - 1);
+              lw[l] = (((lw[l] >> 82 << 32) + $unsigned(mulout) << 50) + tmp2) | 2'b10;
+            end
+            if (8'b01000000 + geni == (lw[l] >> 2) & 8'b11111111 && lw[l] & 1'b1 == 0) begin
+              tmp2 = lw[l] & ((1 << 18) - 1);
+              lw[l] = (((lw[l] >> 50 << 32) + $unsigned(mulout) << 18) + tmp2) | 2'b01;
+            end
+          end
+        for (l = 0; l < 32; l = l + 1) 
+          if (sw[l] >> (`SW_RS_SIZE - 1) == 1) begin
+            if (8'b01000000 + geni == (sw[l] >> 19) & 8'b11111111 && (sw[l] >> 2) & 1'b1 == 0) begin
+              tmp2 = sw[l] & ((1 << 91) - 1);
+              sw[l] = (((sw[l] >> 123 << 32) + $unsigned(mulout) << 91) + tmp2) | 3'b100;
+            end
+            if (8'b01000000 + geni == (sw[l] >> 11) & 8'b11111111 && (tmp >> 1) & 1'b1 == 0) begin
+              tmp2 = sw[l] & ((1 << 59) - 1);
+              sw[l] = (((sw[l] >> 91 << 32) + $unsigned(mulout) << 59) + tmp2) | 3'b010;
+            end
+            if (8'b01000000 + geni == (tmp >> 3) & 8'b11111111 && tmp & 1'b1 == 0) begin
+              tmp2 = sw[l] & ((1 << 27) - 1);
+              sw[l] = (((sw[l] >> 59 << 32) + $unsigned(mulout) << 27) + tmp2) | 3'b001;
+            end
+          end
+        for (l = 0; l < 64; l = l + 1) 
+          if (rrs[l] == 8'b01000000 + geni) begin
+            rrs[l] = 8'b01111111;
+            rf[l] = mulout;
+          end
         mul[geni] = 0;
-        cdbchange = 1;
-      end
-    end
-    always @(cdbchange) begin
-      if (cdbchange && tmp >> (`GENERAL_RS_SIZE - 1) == 1) begin
-        if (cdb >> `WORD_SIZE == (tmp >> 10) & 8'b11111111 && (tmp >> 1) & 1'b1 == 0) begin
-          tmp2 = mul[geni] & ((1 << 50) - 1);
-          mul[geni] = (((mul[geni] >> 82 << 32) + (cdb & `MAX_UNSIGN_INT) << 50) + tmp2) | 2'b10;
-        end
-        if (cdb >> `WORD_SIZE == (tmp >> 2) & 8'b11111111 && tmp & 1'b1 == 0) begin
-          tmp2 = mul[geni] & ((1 << 18) - 1);
-          mul[geni] = (((mul[geni] >> 50 << 32) + (cdb & `MAX_UNSIGN_INT) << 18) + tmp2) | 2'b01;
-        end
-        cdbchange = 0;
       end
     end
   end
@@ -112,12 +198,13 @@ module RS(clk, unit, reg1, reg2, reg3, hasimm, imm, enable, out, regread, regin,
     flush = 0;
   end
   //ALU for lw
-  generate for (geni = 0; geni < 32; geni = geni + 1) begin:czplw
+  generate for (geni = 0; geni < 96; geni = geni + 1) begin:czplw
     wire[`GENERAL_RS_SIZE-1:0] tmp;
     reg signed[`WORD_SIZE-1:0] lwout;
     reg[`GENERAL_RS_SIZE-1:0] tmp2;
     assign tmp = lw[geni];
     wire[`WORD_SIZE-1:0] addres; // add result
+    integer l;
     ADD addd(addres, $signed(tmp[81:50]), $signed(tmp[49:18]));
     always @(posedge clk) begin // need condition
       if (tmp[1:1] == 1'b1 && tmp[0:0] == 1'b1) begin
@@ -127,23 +214,61 @@ module RS(clk, unit, reg1, reg2, reg3, hasimm, imm, enable, out, regread, regin,
           #`CACHE_MISS_TIME lwout = cacheout;
         end else
           lwout = cacheout;
-        readable = 0;
-        cdb = ((8'b10000000 + geni) << `WORD_SIZE) + $unsigned(lwout);
+          
+        for (l = 0; l < 32; l = l + 1) 
+          if (add[l] >> (`GENERAL_RS_SIZE - 1) == 1) begin
+            if (8'b10000000 + geni == (add[l] >> 10) & 8'b11111111 && (add[l] >> 1) & 1'b1 == 0) begin
+              tmp2 = add[l] & ((1 << 50) - 1);
+              add[l] = (((add[l] >> 82 << 32) + $unsigned(lwout) << 50) + tmp2) | 2'b10;
+            end
+            if (8'b10000000 + geni == (add[l] >> 2) & 8'b11111111 && add[l] & 1'b1 == 0) begin
+              tmp2 = add[l] & ((1 << 18) - 1);
+              add[l] = (((add[l] >> 50 << 32) + $unsigned(lwout) << 18) + tmp2) | 2'b01;
+            end
+          end
+        for (l = 0; l < 32; l = l + 1) 
+          if (mul[l] >> (`GENERAL_RS_SIZE - 1) == 1) begin
+            if (8'b10000000 + geni == (mul[l] >> 10) & 8'b11111111 && (mul[l] >> 1) & 1'b1 == 0) begin
+              tmp2 = add[l] & ((1 << 50) - 1);
+              mul[l] = (((mul[l] >> 82 << 32) + $unsigned(lwout) << 50) + tmp2) | 2'b10;
+            end
+            if (8'b10000000 + geni == (mul[l] >> 2) & 8'b11111111 && mul[l] & 1'b1 == 0) begin
+              tmp2 = mul[l] & ((1 << 18) - 1);
+              mul[l] = (((mul[l] >> 50 << 32) + $unsigned(lwout) << 18) + tmp2) | 2'b01;
+            end
+          end
+        for (l = 0; l < 96; l = l + 1) 
+          if (lw[l] >> (`GENERAL_RS_SIZE - 1) == 1) begin
+            if (8'b10000000 + geni == (lw[l] >> 10) & 8'b11111111 && (lw[l] >> 1) & 1'b1 == 0) begin
+              tmp2 = lw[l] & ((1 << 50) - 1);
+              lw[l] = (((lw[l] >> 82 << 32) + $unsigned(lwout) << 50) + tmp2) | 2'b10;
+            end
+            if (8'b10000000 + geni == (lw[l] >> 2) & 8'b11111111 && lw[l] & 1'b1 == 0) begin
+              tmp2 = lw[l] & ((1 << 18) - 1);
+              lw[l] = (((lw[l] >> 50 << 32) + $unsigned(lwout) << 18) + tmp2) | 2'b01;
+            end
+          end
+        for (l = 0; l < 32; l = l + 1) 
+          if (sw[l] >> (`SW_RS_SIZE - 1) == 1) begin
+            if (8'b10000000 + geni == (sw[l] >> 19) & 8'b11111111 && (sw[l] >> 2) & 1'b1 == 0) begin
+              tmp2 = sw[l] & ((1 << 91) - 1);
+              sw[l] = (((sw[l] >> 123 << 32) + $unsigned(lwout) << 91) + tmp2) | 3'b100;
+            end
+            if (8'b10000000 + geni == (sw[l] >> 11) & 8'b11111111 && (tmp >> 1) & 1'b1 == 0) begin
+              tmp2 = sw[l] & ((1 << 59) - 1);
+              sw[l] = (((sw[l] >> 91 << 32) + $unsigned(lwout) << 59) + tmp2) | 3'b010;
+            end
+            if (8'b10000000 + geni == (tmp >> 3) & 8'b11111111 && tmp & 1'b1 == 0) begin
+              tmp2 = sw[l] & ((1 << 27) - 1);
+              sw[l] = (((sw[l] >> 59 << 32) + $unsigned(lwout) << 27) + tmp2) | 3'b001;
+            end
+          end
+        for (l = 0; l < 64; l = l + 1) 
+          if (rrs[l] == 8'b10000000 + geni) begin
+            rrs[l] = 8'b01111111;
+            rf[l] = lwout;
+          end        
         lw[geni] = 0;
-        cdbchange = 1;
-      end
-    end
-    always @(cdbchange) begin
-      if (cdbchange && tmp >> (`GENERAL_RS_SIZE - 1) == 1) begin
-        if (cdb >> `WORD_SIZE == (tmp >> 10) & 8'b11111111 && (tmp >> 1) & 1'b1 == 0) begin
-          tmp2 = lw[geni] & ((1 << 50) - 1);
-          lw[geni] = (((lw[geni] >> 82 << 32) + (cdb & `MAX_UNSIGN_INT) << 50) + tmp2) | 2'b10;
-        end
-        if (cdb >> `WORD_SIZE == (tmp >> 2) & 8'b11111111 && tmp & 1'b1 == 0) begin
-          tmp2 = lw[geni] & ((1 << 18) - 1);
-          lw[geni] = (((lw[geni] >> 50 << 32) + (cdb & `MAX_UNSIGN_INT) << 18) + tmp2) | 2'b01;
-        end
-        cdbchange = 0;
       end
     end
   end
@@ -168,48 +293,10 @@ module RS(clk, unit, reg1, reg2, reg3, hasimm, imm, enable, out, regread, regin,
         sw[geni] = 0;
       end
     end
-    always @(cdbchange) begin
-      if (cdbchange && tmp >> (`SW_RS_SIZE - 1) == 1) begin
-        if (cdb >> `WORD_SIZE == (tmp >> 19) & 8'b11111111 && (tmp >> 2) & 1'b1 == 0) begin
-          tmp2 = sw[geni] & ((1 << 91) - 1);
-          sw[geni] = (((sw[geni] >> 123 << 32) + (cdb & `MAX_UNSIGN_INT) << 91) + tmp2) | 3'b100;
-        end
-        if (cdb >> `WORD_SIZE == (tmp >> 11) & 8'b11111111 && (tmp >> 1) & 1'b1 == 0) begin
-          tmp2 = sw[geni] & ((1 << 59) - 1);
-          sw[geni] = (((sw[geni] >> 91 << 32) + (cdb & `MAX_UNSIGN_INT) << 59) + tmp2) | 3'b010;
-        end
-        if (cdb >> `WORD_SIZE == (tmp >> 3) & 8'b11111111 && tmp & 1'b1 == 0) begin
-          tmp2 = sw[geni] & ((1 << 27) - 1);
-          sw[geni] = (((sw[geni] >> 59 << 32) + (cdb & `MAX_UNSIGN_INT) << 27) + tmp2) | 3'b001;
-        end
-        cdbchange = 0;
-      end
-    end
   end
   endgenerate
   
-  //Register Result Status
-  reg[`UNIT_SIZE-1:0] rrs[63:0];
-  reg[`WORD_SIZE-1:0] rf[63:0];
-  assign regout = rrs[regin];
-  assign regoutrf = rf[regin];
-  initial begin
-    for (k = 0; k < 64; k = k + 1) begin
-      rrs[k] = 8'b01111111;
-      rf[k] = 0;
-    end
-  end
-  reg[`UNIT_SIZE-1:0] rrswrite;
-  always @(cdbchange) begin
-    if (cdbchange) begin
-      rrswrite = cdb >> `WORD_SIZE;
-      for (k = 0; k < 64; k = k + 1) 
-        if (rrs[k] == rrswrite) begin
-          rrs[k] = 8'b01111111;
-          rf[k] = cdb & ((1 << `WORD_SIZE) - 1);
-        end
-    end
-  end
+  
   
   reg halt, over;
   reg[`UNIT_SIZE-1:0] j;
@@ -351,7 +438,7 @@ module RS(clk, unit, reg1, reg2, reg3, hasimm, imm, enable, out, regread, regin,
             add[i] = (((add[i] >> 82 << 32) + rf[reg2] << 50) + tmp2) | 2'b10;
           end
         end
-        rrs[reg1] = i + 8'b10100000;
+        rrs[reg1] = i + 8'b00100000;
         out = 1;
       end
     end
@@ -385,7 +472,7 @@ module RS(clk, unit, reg1, reg2, reg3, hasimm, imm, enable, out, regread, regin,
           end
         end
         $display("put2 mul %b", mul[i]);
-        rrs[reg1] = i + 8'b11000000;
+        rrs[reg1] = i + 8'b01000000;
         out = 1;
       end
     end
@@ -401,6 +488,7 @@ module RS(clk, unit, reg1, reg2, reg3, hasimm, imm, enable, out, regread, regin,
         else begin
                   $display("put mv(add) %b", i);
           add[i] = ((1'b1 << 32 << 32) + reg2 << 8 << 2) + 1'b1;
+          rrs[reg1] = i + 8'b00100000;
           out = 1;
         end
       end else begin
